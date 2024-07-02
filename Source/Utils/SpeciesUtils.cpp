8363ef8bc07c828a5bc5d55df64387c8c3f2e1e5
zeldinmaya <mzeldin@avalanche.energy>
2024-07-02 22:05:50 +0000

    changed input to make mpi looping work

--- Source/Utils/SpeciesUtils.cpp
+++ Source/Utils/SpeciesUtils.cpp
@@ -312,7 +312,8 @@ namespace SpeciesUtils {
     void parseMomentum (std::string const& species_name, std::string const& source_name, const std::string& /*style*/,
         std::unique_ptr<InjectorMomentum,InjectorMomentumDeleter>& h_inj_mom,
         amrex::EBFArrayBoxFactory* field_factory_ptr,
-        amrex::MultiFab* multifab) {
+        amrex::BoxArray* ba, 
+        amrex::DistributionMapping* dm) {
 
         using namespace amrex::literals;
         const amrex::ParmParse pp_species(species_name);
@@ -330,7 +331,7 @@ namespace SpeciesUtils {
             utils::parser::queryWithParser(pp_species, source_name, "un_m",  un_m);
             utils::parser::queryWithParser(pp_species, source_name, "un_th", un_th);
              h_inj_mom.reset(new InjectorMomentum((InjectorMomentumSTLGaussianFlux*)nullptr,
-                                                un_m, un_th, field_factory_ptr, multifab));
+                                                un_m, un_th, field_factory_ptr, ba, dm));
         }
         else {
             std::stringstream stringstream;
