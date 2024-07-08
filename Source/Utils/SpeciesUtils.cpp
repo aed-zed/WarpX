83ad48ebbc244e2883ea789d5494b6143c2193d3
zeldinmaya <mzeldin@avalanche.energy>
2024-07-08 15:46:54 +0000

    changed to use standard vector

--- Source/Utils/SpeciesUtils.cpp
+++ Source/Utils/SpeciesUtils.cpp
@@ -311,8 +311,8 @@ namespace SpeciesUtils {
 #ifdef AMREX_USE_EB
     void parseMomentum (std::string const& species_name, std::string const& source_name, const std::string& /*style*/,
         std::unique_ptr<InjectorMomentum,InjectorMomentumDeleter>& h_inj_mom,
-        const amrex::Vector<amrex::Box>* ba,
-        amrex::Vector<amrex::Array4<const amrex::Real>>* arrays) {
+        const std::Vector<amrex::Box>* ba,
+        std::Vector<amrex::Array4<const amrex::Real>>* arrays) {
 
         using namespace amrex::literals;
         const amrex::ParmParse pp_species(species_name);
