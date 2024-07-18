1d1578e1b7797898ba62ce3bbb95be7bf2db2149
zeldinmaya <mzeldin@avalanche.energy>
2024-07-18 21:57:10 +0000

    printing more messages

--- Source/Particles/ParticleBoundaryBuffer.cpp
+++ Source/Particles/ParticleBoundaryBuffer.cpp
@@ -328,12 +328,14 @@ void ParticleBoundaryBuffer::redistribute () {
 
 const std::vector<std::string>& ParticleBoundaryBuffer::getSpeciesNames() const
 {
+    std::cout << "getting species names" << std::endl; 
     static bool initialized = false;
     if (!initialized)
     {
         const amrex::ParmParse pp_particles("particles");
         pp_particles.queryarr("species_names", m_species_names);
         initialized = true;
+        std::cout << "species names: " << m_species_names << std::endl; 
     }
     return m_species_names;
 }
