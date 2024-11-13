331402a7caf41572f0f5854cfdec13cf7f77ca6b
alex gargone <125412951+agargone@users.noreply.github.com>
2024-11-13 00:15:01 +0000

    Commenting out UpdatePosition to rerun energy error tests

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -1796,7 +1796,7 @@ PhysicalParticleContainer::AddPlasmaFlux (PlasmaInjector const& plasma_injector,
                 // Update particle position by a random `t_fract`
                 // so as to produce a continuous-looking flow of particles
                 const amrex::Real t_fract = amrex::Random(engine)*dt;
-                UpdatePosition(ppos.x, ppos.y, ppos.z, pu.x, pu.y, pu.z, t_fract);
+                //UpdatePosition(ppos.x, ppos.y, ppos.z, pu.x, pu.y, pu.z, t_fract);
 
 #if defined(WARPX_DIM_3D)
                 pa[PIdx::x][ip] = ppos.x;
