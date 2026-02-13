2f1bfff5244dfc94c1ebfa7e428ccb23841644e1
Marco Garten <mgarten@avalanche.energy>
2026-02-12 18:37:55 -0800

    Hybrid-PIC optimizations (squashed)
    Optimizations for Hybrid-PIC solver:
    - CalculatePlasmaCurrent optimization
    - Use 2 ghost cells for E and B field with hybrid-PIC
    - Fill EB update arrays to as many ghost cells as they have
    - Use FillBoundary for EB update array
    - Only increase guard cells for collocated grids
    - Reset checksums
    
    Squashed from hybrid_optimization2 branch for testing.

--- Source/Parallelization/GuardCellManager.cpp
+++ Source/Parallelization/GuardCellManager.cpp
@@ -312,6 +312,13 @@ guardCellManager::Init (
     }
 #endif
 
+    // For the hybrid-PIC solver an extra guard cell is added with collocated grids
+    // to avoid the need for parallel communication after calculating curl x B.
+    if ((electromagnetic_solver_id == ElectromagneticSolverAlgo::HybridPIC) &&
+        (grid_type == GridType::Collocated)) {
+        ng_FieldSolver += 1;
+    }
+
     // Number of guard cells is the max of that determined by particle shape factor and
     // the stencil used in the field solve
     ng_alloc_EB.max( ng_FieldSolver );
